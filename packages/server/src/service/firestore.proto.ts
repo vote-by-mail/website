28f13cd9b1dae5be8ca6013236891c542ece48a2
Luca409 <lucaloncar@Lucas-MBP.nyc.rr.com>
2020-07-05 16:28:30 -0400

    add firestore.proto.ts and make storage.proto.ts work

---
+++ packages/server/src/service/firestore.proto.ts
@@ -0,0 +1,18 @@
+import { RichStateInfo } from './types'
+import { FirestoreService } from './firestore'
+import { processEnvOrThrow } from '../common'
+
+let fs: FirestoreService
+const oid = 'org'
+
+const main = async () => {
+  fs = new FirestoreService(processEnvOrThrow('GCLOUD_PROJECT'))
+  const id = await fs.addRegistration({
+    oid,
+    name: 'Bob',
+    state: 'Florida',
+  } as RichStateInfo)
+  console.log(id)
+}
+
+main()
--- packages/server/src/service/storage.proto.ts
+++ packages/server/src/service/storage.proto.ts
@@ -2,13 +2,12 @@ import { StorageFile } from './storage'
 import fs from 'fs'
 
 const main = async () => {
-  const buffer = fs.readFileSync('test.pdf')
+  const buffer = fs.readFileSync(__dirname + '/test/test.pdf')
   const file = new StorageFile('test/test.pdf')
   const result = await file.upload(buffer)
   console.log(result)
   const reuslt2 = await file.getSignedUrl(24 * 60 * 60 * 1000)
   console.log(reuslt2)
-
 }
 
 main()
