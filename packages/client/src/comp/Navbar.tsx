568024ffb598d907aee33d430b474a62ccd5369e
Lukas Danin <lukas.danin@gmail.com>
2020-07-07 20:29:07 -0300

    Reorder links, update styling for lenghtier links

--- packages/client/src/comp/Navbar.tsx
+++ packages/client/src/comp/Navbar.tsx
@@ -19,22 +19,18 @@ const fadeIn = keyframes`
 
 const slideUp = keyframes`
   from {
-    opacity: 1;
     transform: translateY(calc(var(--height) * 0));
   }
   to {
-    opacity: 0;
     transform: translateY(calc(var(--height) * -1));
   }
 `
 
 const slideDown = keyframes`
   from {
-    opacity: 0;
     transform: translateY(calc(var(--height) * -1));
   }
   to {
-    opacity: 1;
     transform: translateY(calc(var(--height) * 0));
   }
 `
@@ -59,11 +55,11 @@ const Wrapper = styled.div<NavExpanded & { visible: boolean }>`
   }
   ${cssQuery.mobile.tall} {
     ${cssQuery.mobile.wide} {
-      --expandedHeight: 20vh;
+      --expandedHeight: 17vh;
     }
   }
   ${cssQuery.mobile.landscape.all} {
-    --height: 20vh;
+    --height: 17vh;
   }
 
   width: 100%;
@@ -84,21 +80,28 @@ const Wrapper = styled.div<NavExpanded & { visible: boolean }>`
   align-items: flex-start;
   align-content: flex-start;
   justify-content: space-between;
+  ${cssQuery.desktop.all} {
+    align-items: center;
+    justify-content: space-between;
+  }
 
   animation: ${
     p => p.visible
       ? css`${slideDown} ease .25s both`
       : css`${slideUp} ease .25s both`
   };
+  opacity: ${p => p.visible ? '1' : '0'};
 
-  transition: height ease .45s;
+  transition: height ease .45s, opacity ease .2s;
 `
 
 const Logo = styled(Link)`
   height: var(--contentHeight);
-  /* Total space - NavToggle size */
-  width: calc(90% - var(--contentHeight));
-  ${cssQuery.desktop.all} { width: 30%; }
+  /* Total space - NavToggle size - LocaleToggle size */
+  width: calc(90% - var(--contentHeight) - var(--contentHeight));
+  ${cssQuery.desktop.all} {
+    width: unset;
+  }
 
   /* Centers image on the padded, non-expanded area */
   display: flex;
@@ -111,16 +114,20 @@ const Logo = styled(Link)`
   }
 `
 
-const NavToggle = styled(Button)<NavExpanded>`
+const NavToggleButton = styled(Button)<Partial<NavExpanded>>`
   --color: ${p => p.expanded ? '#f44336' : '#2196f3'};
+  --iconRotation: ${p => p.expanded ? '-90deg' : '0'};
   --wrapperSize: var(--contentHeight);
   --iconSize: calc(var(--wrapperSize) * 0.35);
-  --iconRotation: ${p => p.expanded ? '-90deg' : '0'};
+  ${cssQuery.desktop.all} {
+    --wrapperSize: calc(var(--contentHeight) * 0.7);
+    --iconSize: calc(var(--wrapperSize) * 0.4);
+  }
 
   /* In order to make the Material Ripple a perfect circle */
-  width: var(--contentHeight);
-  height: var(--contentHeight);
-  border-radius: var(--contentHeight);
+  width: var(--wrapperSize);
+  height: var(--wrapperSize);
+  border-radius: var(--wrapperSize);
   margin: 0;
 
   /* The Material Ripple already indicates interaction */
@@ -130,8 +137,7 @@ const NavToggle = styled(Button)<NavExpanded>`
   display: flex;
   align-items: center;
   justify-content: center;
-  ${cssQuery.desktop.all} { display: none; }
-
+  padding: 0; margin: 0;
   i {
     font-size: var(--iconSize);
     color: var(--color);
@@ -140,8 +146,13 @@ const NavToggle = styled(Button)<NavExpanded>`
   }
 `
 
+const NavLinksToggle = styled(NavToggleButton)<NavExpanded>`
+  ${cssQuery.desktop.all} { display: none; }
+`
+
 const NavLinks = styled.div<NavExpanded>`
   display: ${p => p.expanded ? 'flex' : 'none'};
+  /* The parent has horizontal padding, this ensures NavLinks is full-width */
   width: 120%;
   height: calc(
     var(--expandedHeight) - var(--contentHeight) - var(--verticalPad) - var(--verticalPad)
@@ -149,6 +160,7 @@ const NavLinks = styled.div<NavExpanded>`
 
   ${cssQuery.mobile.all} {
     flex-direction: column;
+    padding-top: 0.5vh;
 
     box-shadow: 0 4px 3px rgba(0, 0, 0, 0.05) inset;
     /* Makes the shadow (of the navlinks) ignore the Wrapper horizontal padding */
@@ -163,7 +175,7 @@ const NavLinks = styled.div<NavExpanded>`
       the links in one single row.
     */
     ${cssQuery.mobile.wide} {
-      padding: 0 4em;
+      padding: 0.5vh 4em 0;
       flex-direction: row;
       font-size: 0.75em;
     }
@@ -172,15 +184,18 @@ const NavLinks = styled.div<NavExpanded>`
   ${cssQuery.desktop.all} {
     display: flex;
     flex-direction: row;
-    width: 50%;
+    width: unset;
+    min-width: 55%;
     height: 100%;
     font-size: 0.75em;
+    padding-top: 0;
   }
 
   ${cssQuery.mobile.landscape.all} {
     font-size: 0.85em;
     animation: ${fadeIn} ease .5s .3s both;
     flex-direction: row;
+    justify-content: space-around;
   }
 
   justify-content: space-around;
@@ -202,7 +217,7 @@ const NavLinks = styled.div<NavExpanded>`
 
 export const Navbar = () => {
   const { pushStartSection } = useAppHistory()
-  const [expanded, setExpanded] = React.useState(false)
+  const [linksExpanded, setLinksExpanded] = React.useState(false)
   const [visible, setVisibility] = React.useState(true)
   const [scrollY, setScrollY] = React.useState(window.pageYOffset)
 
@@ -215,37 +230,41 @@ export const Navbar = () => {
         }
       } else {
         if (window.pageYOffset > scrollY) {
-          setVisibility(false)
+          if (linksExpanded === false) {
+            setVisibility(false)
+          }
         }
       }
       setScrollY(window.pageYOffset)
     }
-  }, [visible, scrollY])
+  }, [visible, scrollY, linksExpanded])
 
-  const toggleExpanded = () => setExpanded(!expanded)
+  const toggleLinksExpanded = () => {
+    setLinksExpanded(!linksExpanded)
+  }
 
   /** Pushes page section and closes the navbar */
   const pushAndClose = (section: StartSectionPath['type']) => {
     pushStartSection(section)
-    if (expanded) toggleExpanded()
+    if (linksExpanded) toggleLinksExpanded()
   }
 
-  return <Wrapper expanded={expanded} visible={visible}>
+  return <Wrapper expanded={linksExpanded} visible={visible}>
     <Logo to="#" onClick={() => pushAndClose('start')}>
         <img src={logo} alt="VoteByMail"/>
     </Logo>
-    <NavToggle onClick={toggleExpanded} expanded={expanded} variant="flat">
-      <i className={`fa ${expanded ? 'fa-close' : 'fa-navicon'}`}/>
-    </NavToggle>
-    <NavLinks expanded={expanded}>
-      <Link to="#about" onClick={() => pushAndClose('about')}>
-        About
-      </Link>
-      <Link to="#team" onClick={() => pushAndClose('team')}>
-        Team
+    <NavLinksToggle onClick={toggleLinksExpanded} expanded={linksExpanded} variant="flat">
+      <i className={`fa ${linksExpanded ? 'fa-close' : 'fa-navicon'}`}/>
+    </NavLinksToggle>
+    <NavLinks expanded={linksExpanded}>
+      <Link to="#howItWorks" onClick={() => pushAndClose('howItWorks')}>
+        How does it work?
       </Link>
       <Link to="#getInvolved" onClick={() => pushAndClose('getInvolved')}>
         Get Involved
+      </Link>
+      <Link to="#team" onClick={() => pushAndClose('team')}>
+        Team
       </Link>
       <Link to="#contact" onClick={() => pushAndClose('contact')}>
         Contact Us
