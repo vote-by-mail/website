0f7867d514827e1d437b5e53cb70a75e8d246833
Lukas Danin <lukas.danin@gmail.com>
2020-07-09 17:27:28 -0300

    Fix vertical height between checkbox & label

--- packages/client/src/comp/util/Checkbox.tsx
+++ packages/client/src/comp/util/Checkbox.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import Checkbox from 'muicss/lib/react/checkbox'
 import { CheckboxProps } from 'muicss/react'
+import styled from 'styled-components'
 
 import { Red } from './Util'
 
+/** Fixes the vertical displacement of its label */
+export const FixedCheckbox = styled(Checkbox)`
+  label {
+    display: flex;
+    align-items: center;
+    input[type="checkbox"] {
+      margin-top: 1px;
+    }
+  }
+`
+
 export const AppCheckbox = React.forwardRef<Checkbox, CheckboxProps>(function AppCheckbox(props, ref) {
-  return <Checkbox
+  return <FixedCheckbox
     {...props}
     label={<span>{props.label} {props.required ? <Red>*</Red> : null}</span>}
     ref={ref}
