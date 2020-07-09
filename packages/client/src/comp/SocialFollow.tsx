mail-my-ballot/mail-my-ballot

Tweaks icons for new design
Also fixes some issues, such as onClick being on `a` instead of `button`

packages/client/src/comp/SocialFollow.tsx
@@ -1,48 +1,50 @@
 import React from 'react'
 import { SocialButtonWrapper, openInNewWindow } from './SocialShare'
-import { RoundedButton } from './util/Button'
+import { Button } from 'muicss/react'
 
 const FollowTwitter: React.FC = () => {
   const href = 'https://twitter.com/intent/follow?screen_name=mailmyballot'
-  return <SocialButtonWrapper
-    title="Follow @MailMyBallot on Twitter"
-    onClick={() => openInNewWindow(href)}
-  >
-    <RoundedButton color='primary' style={{ backgroundColor: '#00acee' }}>
+  return <SocialButtonWrapper title="Follow @MailMyBallot on Twitter">
+    <Button
+      onClick={() => openInNewWindow(href)}
+      color='primary'
+      style={{backgroundColor: '#00acee'}}
+    >
       <i className="fa fa-twitter"/>
       <span>Follow</span>
-    </RoundedButton>
+    </Button>
   </SocialButtonWrapper>
 }
 
 const FollowInstagram: React.FC = () => {
   const href = 'https://www.instagram.com/mailmyballot'
 
-  return <SocialButtonWrapper
-    title="Follow @MailMyBallot on Instagram"
-    onClick={() => openInNewWindow(href)}
-  >
-    <RoundedButton style={{
-      backgroundImage: 'linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%)',
-      color: 'white',
-      filter: 'brightness(1.1)',
-    }}>
+  return <SocialButtonWrapper title="Follow @MailMyBallot on Instagram">
+    <Button
+      onClick={() => openInNewWindow(href)}
+      style={{
+        backgroundImage: 'linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%)',
+        color: 'white',
+        filter: 'brightness(1.1)',
+      }}
+    >
       <i className="fa fa-instagram"/>
       <span>Follow</span>
-    </RoundedButton>
+    </Button>
   </SocialButtonWrapper>
 }
 
 const FollowFacebook: React.FC = () => {
   const href = 'https://www.facebook.com/Mail-My-Ballot-103074334722159/'
-  return <SocialButtonWrapper
-    title="Follow @MailMyBallot on Facebook"
-    onClick={() => openInNewWindow(href, 1024)}
-  >
-    <RoundedButton color='primary' style={{ backgroundColor: '#3b5998' }}>
+  return <SocialButtonWrapper title="Follow @MailMyBallot on Facebook">
+    <Button
+      onClick={() => openInNewWindow(href, 1024)}
+      color='primary'
+      style={{backgroundColor: '#3b5998'}}
+    >
       <i className="fa fa-facebook"/>
       <span>Follow</span>
-    </RoundedButton>
+    </Button>
   </SocialButtonWrapper>
 }
 

vote-by-mail/website

packages/client/src/comp/SocialFollow.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { SocialButtonWrapper, openInNewWindow } from './SocialShare'
-import { RoundedButton } from './util/Button'
+import { Button } from 'muicss/react'
 
 const FollowTwitter: React.FC = () => {
   const href = 'https://twitter.com/intent/follow?screen_name=votebymailio'
@@ -8,28 +8,28 @@ const FollowTwitter: React.FC = () => {
     title="Follow @VoteByMail on Twitter"
     onClick={() => openInNewWindow(href)}
   >
-    <RoundedButton color='primary' style={{ backgroundColor: '#00acee' }}>
+    <Button color='primary' style={{ backgroundColor: '#00acee' }}>
       <i className="fa fa-twitter"/>
       <span>Follow</span>
-    </RoundedButton>
+    </Button>
   </SocialButtonWrapper>
 }
 
 const FollowInstagram: React.FC = () => {
   const href = 'https://www.instagram.com/votebymail.io'
 
-  return <SocialButtonWrapper
-    title="Follow @VoteByMail on Instagram"
-    onClick={() => openInNewWindow(href)}
-  >
-    <RoundedButton style={{
-      backgroundImage: 'linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%)',
-      color: 'white',
-      filter: 'brightness(1.1)',
-    }}>
+  return <SocialButtonWrapper title="Follow @VoteByMail on Instagram">
+    <Button
+      onClick={() => openInNewWindow(href)}
+      style={{
+        backgroundImage: 'linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%)',
+        color: 'white',
+        filter: 'brightness(1.1)',
+      }}
+    >
       <i className="fa fa-instagram"/>
       <span>Follow</span>
-    </RoundedButton>
+    </Button>
   </SocialButtonWrapper>
 }
 
@@ -39,10 +39,10 @@ const FollowFacebook: React.FC = () => {
     title="Follow @VoteByMail on Facebook"
     onClick={() => openInNewWindow(href, 1024)}
   >
-    <RoundedButton color='primary' style={{ backgroundColor: '#3b5998' }}>
+    <Button color='primary' style={{ backgroundColor: '#3b5998' }}>
       <i className="fa fa-facebook"/>
       <span>Follow</span>
-    </RoundedButton>
+    </Button>
   </SocialButtonWrapper>
 }
 