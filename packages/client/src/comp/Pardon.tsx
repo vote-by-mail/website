21346663338bc19bc3c83e92387297946d41eb30
Luca409 <lucaloncar@Lucas-MBP.nyc.rr.com>
2020-07-09 00:28:28 -0400

    fix pardon page incase we need it

--- packages/client/src/comp/Pardon.tsx
+++ packages/client/src/comp/Pardon.tsx
@@ -4,6 +4,6 @@ import Container from 'muicss/lib/react/container'
 export const Pardon = () => (<Container>
   <h1>Pardon Our Apperance</h1>
   <p>
-   FILL
+  This site is temporarily down and we are working to get it back in service as quickly as possible.
   </p>
 </Container>)
