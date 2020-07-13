0e7fd84dd127abeb5931c148943eb5b07f63d1ed
Lukas Danin <lukas.danin@gmail.com>
2020-07-13 13:06:09 -0300

    Implements UnderlineLink

---
+++ packages/client/src/comp/util/UnderlineLink.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+
+/**
+ * Creates an HTML Anchor element where its underline is padded by a few
+ * pixels from the text.
+ */
+export const UnderlineLink = styled(Link)`
+  text-transform: uppercase;
+  font-weight: bold;
+  font-size: 12px;
+  padding-bottom: 0.2em;
+  border-bottom: 2px solid #2196F3;
+  &:hover {
+    text-decoration: none;
+  }
+`
