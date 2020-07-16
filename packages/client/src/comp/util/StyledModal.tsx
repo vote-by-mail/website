598c05052112e10631e32c6d5298a9817c0efbe7
Lukas Danin <lukas.danin@gmail.com>
2020-07-15 23:23:29 -0300

    Limits max-size, animates entrance

--- packages/client/src/comp/util/StyledModal.tsx
+++ packages/client/src/comp/util/StyledModal.tsx
@@ -1,17 +1,36 @@
 import Modal from 'styled-react-modal'
+import { keyframes } from 'styled-components'
+
+const slideAndFade = keyframes`
+  from {
+    transform: translateY(-100%);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0);
+    opacity: 1;
+  }
+`
 
 export const StyledModal = Modal.styled`
   top: 50%;
   left: 50%;
   right: auto;
   bottom: auto;
+
   width: 50%;
-  height: 100%;
-  overflow: scroll;
+  min-height: 500px;
+  max-height: 60vh;
+  /* Hides unnecessary scrollbars */
+  overflow: auto;
   background-color: white;
+  border-radius: 4px;
   padding: 40px;
+
   @media only screen and (max-width: 544px) {
     padding: 20px;
     width: 80%;
   }
+
+  animation: ${slideAndFade} ease .4s both;
 `
