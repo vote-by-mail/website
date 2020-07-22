b888f627f3d5c5851783e0889ea75d9fed5a063e
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-07-21 20:57:13 -0700

    Fix proto

--- packages/server/src/service/pdfForm/pdfForm.proto.ts
+++ packages/server/src/service/pdfForm/pdfForm.proto.ts
@@ -1,12 +1,12 @@
 import fs from 'fs'
 import { toSignatureBuffer } from './util'
-import { stateInfo } from '../letter/router'
+import { sampleStateInfo } from '../letter'
 import { MassachusettsInfo } from '../../common'
 import { fillMassachusetts } from './massachusetts'
 
 
 const main = async () => {
-  const data = await stateInfo('Massachusetts') as MassachusettsInfo
+  const data = await sampleStateInfo('Massachusetts') as MassachusettsInfo
   const buffer = await fillMassachusetts({...data})
   fs.writeFileSync('/tmp/Foo.pdf', buffer)
   const signatureBuffer = await toSignatureBuffer(data.signature, 200, 50)
