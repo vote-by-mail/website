2803e24701e2df5dbc96621804dc14b74a149e57
Lukas Danin <lukas.danin@gmail.com>
2020-07-25 14:28:28 -0300

    Removes age restriction

--- packages/client/src/comp/states/BaseContainer.ts
+++ packages/client/src/comp/states/BaseContainer.ts
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { createContainer } from 'unstated-next'
 
-const datePattern = /^(0[1-9]|1[012])[/.](0[1-9]|[12][0-9]|3[01])[/.](19[0-9]{2}|200[0-2])$/i
+const datePattern = /(0[1-9]|1[012])[/.](0[1-9]|[12][0-9]|3[01])[/.](19|20)[0-9]{2}/i
 // eslint-disable-next-line no-useless-escape
 const emailPattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
 const telephonePattern = /[0-9]{3}-?[0-9]{3}-?[0-9]{4}/
