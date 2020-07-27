8a212cf50982f5bbe0da0af34a70273a73ddd7a1
Marco Carvalho <marcoaureliocarvalhofilho@hotmail.com>
2020-07-27 19:44:56 -0300

    function to format phone number into e164

--- packages/common/util.ts
+++ packages/common/util.ts
@@ -6,3 +6,12 @@ export type WithoutId<T extends {}> = Omit<T, 'id'>
 export type WithId<T extends {}> = T & {id: string}
 
 export const wait = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+
+export const formatPhoneNumber = (phoneNumberString: string) => {
+  const cleaned = ('' + phoneNumberString).replace(/\D/g, '')
+  const match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/)
+  if (match) {
+    return '(' + match[1] + ') ' + match[2] + '-' + match[3]
+  }
+  return phoneNumberString
+}
