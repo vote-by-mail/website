7ba87af2e0d83e29a1f52cebb7bd5a076209042b
Marco Carvalho <marcoaureliocarvalhofilho@hotmail.com>
2020-07-28 18:20:11 -0300

    Combine TWILIO_DIVERT and RECEIVE_FAX_NUMBER to DIVERT_FAX_NUMBER

--- packages/server/src/service/twilio.proto.ts
+++ packages/server/src/service/twilio.proto.ts
@@ -1,11 +1,12 @@
 import { StorageFile } from './storage'
 import { sendFaxes } from './twilio'
+import { processEnvOrThrow } from '../common'
 
 const main = async () => {
   const file = new StorageFile('test/test.pdf')
   const uri = await file.getSignedUrl(24 * 60 * 60 * 1000)
   console.log(uri)
-  const faxInstance = await sendFaxes(uri, [process.env['RECEIVE_FAX_NUMBER']])
+  const faxInstance = await sendFaxes(uri, [processEnvOrThrow('DIVERT_FAX_NUMBER')])
   console.log(faxInstance)
   return faxInstance
 }
