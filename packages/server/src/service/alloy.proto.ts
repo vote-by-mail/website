2b9737acdbdd38e5c4c2b89b8eb212e26c4eeb0f
Lukas Danin <lukas.danin@gmail.com>
2020-07-28 14:34:36 -0300

    Tests for more users on the Sandbox

--- packages/server/src/service/alloy.proto.ts
+++ packages/server/src/service/alloy.proto.ts
@@ -19,6 +19,78 @@ const inactiveVoter: RegistrationArgs = {
   }
 }
 
+const notFoundVoter: RegistrationArgs = {
+  firstName: 'Brandie', lastName: 'Nguyen', birthdate: '1946-12-11',
+  address: {
+    stateAbbr: 'AK', city: 'Wasilla', postcode: '99654',
+    streetNumber: '9015', street: 'Fairview St',
+  },
+}
+
+const cancelledVoter: RegistrationArgs = {
+  firstName: 'Peggy', lastName: 'Lee', birthdate: '1993-11-26',
+  address: {
+    stateAbbr: 'AZ', city: 'Tucson', postcode: '85718',
+    streetNumber: '5737', street: 'Homestead Rd',
+  },
+}
+
+const purgedVoter: RegistrationArgs = {
+  firstName: 'Rebecca', lastName: 'Jones', birthdate: '1946-12-22',
+  address: {
+    stateAbbr: 'CT', city: 'Vernon Rockville', postcode: '06066',
+    streetNumber: '8135', street: 'Woodland St',
+  },
+}
+
+const notReportedVoter: RegistrationArgs = {
+  firstName: 'Catherine', lastName: 'Medina', birthdate: '1977-03-01',
+  address: {
+    stateAbbr: 'IN', city: 'Brownsburg', postcode: '46112',
+    streetNumber: '7438', street: 'W Sherman Dr',
+  },
+}
+
+const pendingVoter: RegistrationArgs = {
+  firstName: 'Cecil', lastName: 'Flores', birthdate: '1979-10-13',
+  address: {
+    stateAbbr: 'IA', city: 'Ankeny', postcode: '50023',
+    streetNumber: '1343', street: 'Smokey Ln',
+  },
+}
+
+const provisionalVoter: RegistrationArgs = {
+  firstName: 'Rhonda', lastName: 'Jordan', birthdate: '1992-11-15',
+  address: {
+    stateAbbr: 'MT', city: 'Butte', postcode: '59701',
+    streetNumber: '4498', street: 'Northaven Rd',
+  },
+}
+
+const preregisteredVoter: RegistrationArgs = {
+  firstName: 'Guy', lastName: 'Graves', birthdate: '2003-06-01',
+  address: {
+    stateAbbr: 'NY', city: 'Massapequa Park', postcode: '11762',
+    streetNumber: '5851', street: 'W Belt Line Rd',
+  },
+}
+
+const removedVoter: RegistrationArgs = {
+  firstName: 'George', lastName: 'Woods', birthdate: '1990-07-30',
+  address: {
+    stateAbbr: 'NC', city: 'Elizabeth City', postcode: '27909',
+    streetNumber: '1101', street: 'Karen Dr',
+  },
+}
+
+const challengedVoter: RegistrationArgs = {
+  firstName: 'Diana', lastName: 'Washington', birthdate: '1970-08-24',
+  address: {
+    stateAbbr: 'VT', city: 'Ascutney', postcode: '05030',
+    streetNumber: '6117', street: 'Paddock Way',
+  },
+}
+
 const testStatus = async (voter: RegistrationArgs) => {
   const name = `${voter.firstName} ${voter.lastName}`
   console.log(`Detecting if ${name} can vote.`)
@@ -34,9 +106,22 @@ const testStatus = async (voter: RegistrationArgs) => {
 const main = async () => {
   await testStatus(activeVoter)
   await testStatus(inactiveVoter)
-  // Only needed to test if ALLOY_MOCK
-  if (process.env.ALLOY_MOCK) {
+  await testStatus(notFoundVoter)
+  await testStatus(cancelledVoter)
+  await testStatus(purgedVoter)
+  await testStatus(notReportedVoter)
+  await testStatus(pendingVoter)
+  await testStatus(provisionalVoter)
+  await testStatus(preregisteredVoter)
+  await testStatus(removedVoter)
+
+  await testStatus(challengedVoter)
+  // Only needed to test if ALLOY_RELAXED
+  if (process.env.ALLOY_RELAXED) {
     activeVoter.firstName = 'Unregistered'
+    activeVoter.lastName = 'Voter'
+    await testStatus(activeVoter)
+    activeVoter.firstName = 'Not Found'
     activeVoter.lastName = 'Voter'
     await testStatus(activeVoter)
   }
