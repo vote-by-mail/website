2f3dcb9921efbaaf44405e8e7fa7eb11605de99f
Michael Li <tianhuil@cs.princeton.edu>
2020-07-28 09:49:13 -0700

    Update env.statePrimary.js

--- env/env.statePrimary.js
+++ env/env.statePrimary.js
@@ -1,7 +1,8 @@
 const statePrimary = {
   REACT_APP_GEORGIA_PRIMARY: undefined,
-  REACT_APP_MASSACHUSETTS_PRIMARY: undefined,
-  REACT_APP_NEW_HAMPSHIRE_PRIMARY: undefined,
+  REACT_APP_ARIZONA_PRIMARY: 1,
+  REACT_APP_MASSACHUSETTS_PRIMARY: 1,
+  REACT_APP_NEW_HAMPSHIRE_PRIMARY: 1,
 }
 
 module.exports = { statePrimary }
