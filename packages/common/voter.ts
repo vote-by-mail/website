09958b46baf15f8f49f22e12d0763648e1e4e02f
Lukas Danin <lukas.danin@gmail.com>
2020-07-28 13:22:55 -0300

    Documents source of registration status
    Also adds missing API status from
    https://docs.alloy.us/api/#section/Sandbox/Getting-Started and Not Found

--- packages/common/voter.ts
+++ packages/common/voter.ts
@@ -13,10 +13,18 @@ export interface Voter extends UTM {
   uid: string
 }
 
+// Most values were taken from Alloy API:
+// https://docs.alloy.us/api/#/paths/~1v1~1voters/get
+//
+// Some additional values were taken from this section of the API:
+// https://docs.alloy.us/api/#section/Sandbox/Getting-Started
 export type RegistrationStatus =
   | 'Active'
   | 'Challenged'
   | 'Not Eligible'
+  // Not from the Alloy API, but we use it here since the API returns
+  // null on these cases
+  | 'Not Found'
   | 'Denied'
   | 'Inactive'
   | 'Pending'
@@ -29,6 +37,9 @@ export type RegistrationStatus =
   | 'Cancelled'
   | 'Incomplete'
   | 'Purged'
+  | 'Not Reported'
+  | 'Suspense'
+  | 'Provisional'
   | null
 
 export interface RegistrationArgs {
