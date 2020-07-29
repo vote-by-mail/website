84c5732bad640d9761e083a8c1dd9b1a86cbf1a6
Lukas Danin <lukas.danin@gmail.com>
2020-07-29 16:23:39 -0300

    Implements mockVoter

--- packages/server/src/service/alloy.test.ts
+++ packages/server/src/service/alloy.test.ts
@@ -1,114 +1,100 @@
 import { isRegistered } from './alloy'
-import { RegistrationArgs } from '../common'
+import { RegistrationArgs, RegistrationStatus } from '../common'
 import { testEach } from './utilTests'
 
-// Values taken from https://docs.alloy.us/api/#section/Sandbox/Getting-Started
 
 const alloyMock = process.env.ALLOY_MOCK
 
-const voters = [
-  {
+type RegistrationArgsWithStatus = RegistrationArgs & { status: RegistrationStatus }
+
+const mockVoter = (voter: RegistrationArgsWithStatus): RegistrationArgsWithStatus => ({
+  ...voter,
+  firstName: !alloyMock ? voter.firstName : voter.status,
+  lastName: !alloyMock ? voter.lastName : 'Voter',
+})
+
+// Values taken from https://docs.alloy.us/api/#section/Sandbox/Getting-Started
+const voters: RegistrationArgsWithStatus[] = [
+  mockVoter({
     status: 'Active',
-    firstName: alloyMock ? 'Active' : 'Terrence',
-    lastName: alloyMock ? 'Voter' : 'Brown',
+    firstName: 'Terrence',
+    lastName: 'Brown',
     birthdate: '1963-05-05',
-    address: {
-      stateAbbr: 'DC', city: 'Washington', postcode: '20020',
-      street: 'Pecan Acres Ln', streetNumber: '2969',
-    }
-  }, {
+    stateAbbr: 'DC', city: 'Washington', postcode: '20020',
+    street: 'Pecan Acres Ln', streetNumber: '2969',
+  }), mockVoter({
     status: 'Inactive',
-    firstName: alloyMock ? 'Inactive' : 'Mia',
-    lastName: alloyMock ? 'Voter' : 'Jackson',
+    firstName: 'Mia',
+    lastName: 'Jackson',
     birthdate: '1996-12-21',
-    address: {
-      stateAbbr: 'DE', city: 'Bear', postcode: '19701',
-      street: 'Oak Lawn Ave', streetNumber: '1067',
-    }
-  }, {
+    stateAbbr: 'DE', city: 'Bear', postcode: '19701',
+    street: 'Oak Lawn Ave', streetNumber: '1067',
+  }), mockVoter({
     status: 'Not Found',
-    firstName: alloyMock ? 'Not Found' : 'Brandie',
-    lastName: alloyMock ? 'Voter' : 'Nguyen',
+    firstName: 'Brandie',
+    lastName: 'Nguyen',
     birthdate: '1946-12-11',
-    address: {
-      stateAbbr: 'AK', city: 'Wasilla', postcode: '99654',
-      streetNumber: '9015', street: 'Fairview St',
-    },
-  }, {
+    stateAbbr: 'AK', city: 'Wasilla', postcode: '99654',
+    streetNumber: '9015', street: 'Fairview St',
+  }), mockVoter({
     status: 'Cancelled',
-    firstName: alloyMock ? 'Cancelled' : 'Peggy',
-    lastName: alloyMock ? 'Voter' : 'Lee',
+    firstName: 'Peggy',
+    lastName: 'Lee',
     birthdate: '1993-11-26',
-    address: {
-      stateAbbr: 'AZ', city: 'Tucson', postcode: '85718',
-      streetNumber: '5737', street: 'Homestead Rd',
-    },
-  }, {
+    stateAbbr: 'AZ', city: 'Tucson', postcode: '85718',
+    streetNumber: '5737', street: 'Homestead Rd',
+  }), mockVoter({
     status: 'Purged',
-    firstName: alloyMock ? 'Purged' : 'Rebecca',
-    lastName: alloyMock ? 'Voter' : 'Jones',
+    firstName: 'Rebecca',
+    lastName: 'Jones',
     birthdate: '1946-12-22',
-    address: {
-      stateAbbr: 'CT', city: 'Vernon Rockville', postcode: '06066',
-      streetNumber: '8135', street: 'Woodland St',
-    },
-  }, {
+    stateAbbr: 'CT', city: 'Vernon Rockville', postcode: '06066',
+    streetNumber: '8135', street: 'Woodland St',
+  }), mockVoter({
     status: 'Not Reported',
-    firstName: alloyMock ? 'Not Reported' : 'Catherine',
-    lastName: alloyMock ? 'Voter' : 'Medina',
+    firstName: 'Catherine',
+    lastName: 'Medina',
     birthdate: '1977-03-01',
-    address: {
-      stateAbbr: 'IN', city: 'Brownsburg', postcode: '46112',
-      streetNumber: '7438', street: 'W Sherman Dr',
-    },
-  }, {
+    stateAbbr: 'IN', city: 'Brownsburg', postcode: '46112',
+    streetNumber: '7438', street: 'W Sherman Dr',
+  }), mockVoter({
     status: 'Pending',
-    firstName: alloyMock ? 'Pending' : 'Cecil',
-    lastName: alloyMock ? 'Voter' : 'Flores',
+    firstName: 'Cecil',
+    lastName: 'Flores',
     birthdate: '1979-10-13',
-    address: {
-      stateAbbr: 'IA', city: 'Ankeny', postcode: '50023',
-      streetNumber: '1343', street: 'Smokey Ln',
-    },
-  }, {
+    stateAbbr: 'IA', city: 'Ankeny', postcode: '50023',
+    streetNumber: '1343', street: 'Smokey Ln',
+  }), mockVoter({
     status: 'Provisional',
-    firstName: alloyMock ? 'Provisional' : 'Rhonda',
-    lastName: alloyMock ? 'Voter' : 'Jordan',
+    firstName: 'Rhonda',
+    lastName: 'Jordan',
     birthdate: '1992-11-15',
-    address: {
-      stateAbbr: 'MT', city: 'Butte', postcode: '59701',
-      streetNumber: '4498', street: 'Northaven Rd',
-    },
-  }, {
+    stateAbbr: 'MT', city: 'Butte', postcode: '59701',
+    streetNumber: '4498', street: 'Northaven Rd',
+  }), mockVoter({
     status: 'Preregistered',
-    firstName: alloyMock ? 'Preregistered' : 'Guy',
-    lastName: alloyMock ? 'Voter' : 'Graves',
+    firstName: 'Guy',
+    lastName: 'Graves',
     birthdate: '2003-06-01',
-    address: {
-      stateAbbr: 'NY', city: 'Massapequa Park', postcode: '11762',
-      streetNumber: '5851', street: 'W Belt Line Rd',
-    },
-  }, {
+    stateAbbr: 'NY', city: 'Massapequa Park', postcode: '11762',
+    streetNumber: '5851', street: 'W Belt Line Rd',
+  }), mockVoter({
     status: 'Removed',
-    firstName: alloyMock ? 'Removed' : 'George',
-    lastName: alloyMock ? 'Voter' : 'Woods',
+    firstName: 'George',
+    lastName: 'Woods',
     birthdate: '1990-07-30',
-    address: {
-      stateAbbr: 'NC', city: 'Elizabeth City', postcode: '27909',
-      streetNumber: '1101', street: 'Karen Dr',
-    },
-  }, {
+    stateAbbr: 'NC', city: 'Elizabeth City', postcode: '27909',
+    streetNumber: '1101', street: 'Karen Dr',
+  }), mockVoter({
     status: 'Challenged',
-    firstName: alloyMock ? 'Challenged' : 'Diana',
-    lastName: alloyMock ? 'Voter' : 'Washington',
+    firstName: 'Diana',
+    lastName: 'Washington',
     birthdate: '1970-08-24',
-    address: {
-      stateAbbr: 'VT', city: 'Ascutney', postcode: '05030',
-      streetNumber: '6117', street: 'Paddock Way',
-    },
-  }
+    stateAbbr: 'VT', city: 'Ascutney', postcode: '05030',
+    streetNumber: '6117', street: 'Paddock Way',
+  })
 ]
 
 testEach(voters)('Alloy %s Voter', async (voter) => {
-  expect(await isRegistered(voter as unknown as RegistrationArgs, true)).toBe(voter.status)
+  expect(await isRegistered(voter, true)).toBe(voter.status)
 })
