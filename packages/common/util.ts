9f9935cf82c544f5f6df298e77fcf9b1f1ca1b96
Marco Carvalho <marcoaureliocarvalhofilho@hotmail.com>
2020-07-29 18:48:19 -0300

    Code review changes

--- packages/common/util.ts
+++ packages/common/util.ts
@@ -7,17 +7,17 @@ export type WithId<T extends {}> = T & {id: string}
 
 export const wait = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 
-export const formatPhoneNumber = (phoneNumberString: string) => {
-  const cleaned = ('' + phoneNumberString).replace(/\D/g, '')
+export const formatPhoneNumber = (phoneNumberString: string): string => {
+  const cleaned = phoneNumberString.replace(/^\+[0-9]/, '').replace(/[^a-zA-Z0-9]/g,'')
   const validPhoneNumber = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/)
   let formattedPhoneNumber
   if (validPhoneNumber) {
     formattedPhoneNumber = '(' + validPhoneNumber[1] + ') ' + validPhoneNumber[2] + '-' + validPhoneNumber[3]
   }
   
-  const match = formattedPhoneNumber.match(/^(\([0-9]{3}\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}$/)
+  const match = formattedPhoneNumber && formattedPhoneNumber.match(/^(\([0-9]{3}\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}$/)
 
-  if(match){
+  if(match && formattedPhoneNumber){
     return formattedPhoneNumber
   } else {
     throw new Error(`Invalid Phone Number`)
