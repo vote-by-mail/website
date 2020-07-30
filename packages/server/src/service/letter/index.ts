3be332336a7c573911202bcfd09171b91b83e59c
Marco Carvalho <marcoaureliocarvalhofilho@hotmail.com>
2020-07-30 16:41:42 -0300

    Wrong import

--- packages/server/src/service/letter/index.ts
+++ packages/server/src/service/letter/index.ts
@@ -5,7 +5,6 @@ import nunjucks from 'nunjucks'
 import { processEnvOrThrow, StateInfo, ContactMethod, ImplementedState } from '../../common'
 import { fillMinnesota, fillNewHampshire, fillNorthCarolina, fillMassachusetts, fillVirginia } from '../pdfForm'
 import { staticDir } from '../util'
-import { ConnectionPolicyTargetContext } from 'twilio/lib/rest/voice/v1/connectionPolicy/connectionPolicyTarget'
 
 export const mg = mailgun({
   domain: processEnvOrThrow('MG_DOMAIN'),
