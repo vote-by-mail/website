473df7f46d921af3dd095ac3fc5f914e814b5d86
Lukas Danin <lukas.danin@gmail.com>
2020-08-05 19:19:39 -0300

    Updates cache based on ISO-8601 week number

--- packages/server/src/service/util.ts
+++ packages/server/src/service/util.ts
@@ -50,12 +50,60 @@ export const safeReadFileSync = (
   options?: { encoding?: null | undefined, flag?: string | undefined },
 ) => copyBuffer(fs.readFileSync(path, options))
 
+
+/**
+ * Returns a string indicating the current week number. Since this function
+ * follows ISO-8601, the first week of January might actually belong to
+ * the previous year, which is why this function returns a string formatted
+ * as `YYYY.WN` where YYYY is the year and WN the weeknumber.
+ *
+ * @param d exported for testing purposes, defaults to now when not present
+ *
+ * Based on: https://stackoverflow.com/a/6117889/9611230
+ */
+export const weekNumber = (d: Date = new Date()): string => {
+  const date = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()))
+
+  // Set to nearest Thursday: current date + 4 - current day number
+  // Make Sunday's day number 7
+  date.setUTCDate(date.getUTCDate() + 4 - (date.getUTCDay()||7))
+
+  const firstDayOfYear = new Date(Date.UTC(date.getUTCFullYear(), 0, 1))
+  // Calculate full weeks to nearest Thursday
+  const number = Math.ceil(
+    (((date.valueOf() - firstDayOfYear.valueOf()) / 86400000) + 1) / 7
+  )
+
+  // If the month is January and week number is above 5, it belongs to the
+  // previous year
+  const previousYear = date.getUTCMonth() === 0 && number > 5
+  const year = previousYear ? date.getUTCFullYear() - 1 : date.getUTCFullYear()
+
+  return `${year}.${number}`
+}
+
+// Returns a boolean indicating if the cache should be updated, also updates
+// the cacheVer file when needed.
+const handleCacheVer = (options?: Options): boolean => {
+  const yearWeek = weekNumber()
+  const workDir = options?.workDir ?? `${__dirname}/cache`
+  const path = `${workDir}/cacheVer`
+  const encoding = 'utf-8'
+
+  if (fs.existsSync(path)) {
+    if(fs.readFileSync(path, { encoding }) === yearWeek) return false
+  }
+
+  fs.writeFileSync(path, yearWeek, { encoding })
+  return true
+}
+
 export const cache = <A extends unknown[], R>(
   func: AsyncFunc<A, R>,
   namer: AsyncFunc<A, string>,
   options?: Options,
 ): AsyncFunc<A, R> => {
-  const refresh   = options?.refresh  ?? false,
+  const refresh   = options?.refresh  ?? handleCacheVer(),
         workDir   = options?.workDir  ?? `${__dirname}/cache/`,
         encoding  = options?.encoding ?? 'utf-8'
 
