1f0a086407228312fa920906b9ffc8fdde59f3f3
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-08-10 08:45:17 -0700

    make joining more clear

--- packages/server/src/service/pdfForm/util.ts
+++ packages/server/src/service/pdfForm/util.ts
@@ -32,7 +32,14 @@ export const splitFullName = (fullName: string) => {
 
 export const stripPhoneNumber = (phoneNumber: string) => {
   // Get rid of characters. split-join acts as 'replaceAll'
-  let strippedNumber = phoneNumber.replace('+', '').replace('(', '').replace(')', '').split('-').join('').split(' ').join('')
+  let strippedNumber = phoneNumber
+    .replace('+', '')
+    .replace('(', '')
+    .replace(')', '')
+    .split('-')
+    .join('')
+    .split(' ')
+    .join('')
   // Remove leading 1.
   if (strippedNumber.length > 10) {
     strippedNumber = strippedNumber.substr(1, strippedNumber.length)
