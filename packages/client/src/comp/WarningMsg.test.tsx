4a39273b13261e4d29b1115ca57c87b183aa6af2
Lukas Danin <lukas.danin@gmail.com>
2020-08-10 19:16:02 -0300

    Writes tests for the first visit warning modal

---
+++ packages/client/src/comp/WarningMsg.test.tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import { render, fireEvent, act } from '@testing-library/react'
+import { WarningMsg } from './WarningMsg'
+import { UnstatedContainer } from "./StateContainer"
+
+
+const renderResult = () => render(
+  <WarningMsg/>,
+  { wrapper: UnstatedContainer }
+)
+
+// When visiting the page for the first time
+
+test('Modal shows by default when visiting the page for the first time', () => {
+  localStorage.removeItem('visited')
+
+  const rendered = renderResult()
+
+  const modal = rendered.getByTestId('notProductionModal')
+
+  expect(modal).toBeInTheDocument()
+})
+test('Dismissing the modal triggers a toast when visiting the page for the first time', async () => {
+  localStorage.removeItem('visited')
+
+  const rendered = renderResult()
+
+  const dismissButton = rendered.getByTestId('notProductionButton')
+
+  await act(async () => {
+    await fireEvent.click(dismissButton)
+    // The modal has a small delay to hide due to its animations
+    await new Promise(r => setTimeout(() => r(), 800))
+  })
+
+  const toast = rendered.getByText('You can toggle this popup again by clicking on "Not Production" at the bottom right corner of the page.')
+
+  expect(toast).toBeInTheDocument()
+})
+
+// When visiting the page again
+
+test('Modal doesn\'t show by default when visiting the page again', () => {
+  localStorage.setItem('visited', 'true')
+
+  const rendered = renderResult()
+
+  const modal = rendered.queryByTestId('notProductionModal')
+
+  expect(modal).toBe(null)
+})
+
+test('Dismissing the modal doesn\'t trigger toasts when visiting the page again', async () => {
+  localStorage.setItem('visited', 'true')
+
+  const rendered = renderResult()
+
+  const dismissButton = rendered.getByTestId('notProductionButton')
+
+  await act(async () => {
+    await fireEvent.click(dismissButton)
+    // The modal has a small delay to hide due to its animations
+    await new Promise(r => setTimeout(() => r(), 800))
+  })
+
+  const toast = rendered.queryByText('You can toggle this popup again by clicking on "Not Production" at the bottom right corner of the page.')
+
+  expect(toast).toBe(null)
+})
