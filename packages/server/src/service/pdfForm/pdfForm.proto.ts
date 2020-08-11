bf813275bcffd6d951d1b73e654a825dc0b248f0
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-08-10 20:24:31 -0700

    Fix New Hampshire party

--- packages/server/src/service/pdfForm/pdfForm.proto.ts
+++ packages/server/src/service/pdfForm/pdfForm.proto.ts
@@ -1,13 +1,13 @@
 import fs from 'fs'
 import { toSignatureBuffer } from './util'
 import { sampleStateInfo } from '../letter'
-import { VirginiaInfo } from '../../common'
-import { fillVirginia } from './virginia'
+import { NewHampshireInfo } from '../../common'
+import { fillNewHampshire } from './newHampshire'
 
 
 const main = async () => {
-  const data = await sampleStateInfo('Virginia') as VirginiaInfo
-  const buffer = await fillVirginia({...data})
+  const data = await sampleStateInfo('New Hampshire') as NewHampshireInfo
+  const buffer = await fillNewHampshire({...data})
   fs.writeFileSync('/tmp/Foo.pdf', buffer)
   const signatureBuffer = await toSignatureBuffer(data.signature, 200, 50)
   fs.writeFileSync('/tmp/Bar.pdf', signatureBuffer)
