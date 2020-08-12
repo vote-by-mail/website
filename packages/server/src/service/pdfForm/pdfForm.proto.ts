428b1b4580bf94e88899b7f7349ff8dafeeb545f
Lukas Danin <lukas.danin@gmail.com>
2020-08-11 21:57:27 -0300

    Improves sampleStateInfo type inference
    Always returns the same type of StateInfo as the one used as param

--- packages/server/src/service/pdfForm/pdfForm.proto.ts
+++ packages/server/src/service/pdfForm/pdfForm.proto.ts
@@ -1,13 +1,12 @@
 import fs from 'fs'
 import { toSignatureBuffer } from './util'
 import { sampleStateInfo } from '../letter'
-import { NewHampshireInfo } from '../../common'
-import { fillNewHampshire } from './newHampshire'
+import { fillVirginia } from './virginia'
 
 
 const main = async () => {
-  const data = await sampleStateInfo('New Hampshire') as NewHampshireInfo
-  const buffer = await fillNewHampshire({...data})
+  const data = await sampleStateInfo('Virginia')
+  const buffer = await fillVirginia({...data})
   fs.writeFileSync('/tmp/Foo.pdf', buffer)
   const signatureBuffer = await toSignatureBuffer(data.signature, 200, 50)
   fs.writeFileSync('/tmp/Bar.pdf', signatureBuffer)
