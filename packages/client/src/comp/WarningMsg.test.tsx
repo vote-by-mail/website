8dd1acdeaac0b72502e3b95bb1729936a31c180f
Lukas Danin <lukas.danin@gmail.com>
2020-08-12 23:40:48 -0300

    Simplifies tests

--- packages/client/src/comp/WarningMsg.test.tsx
+++ packages/client/src/comp/WarningMsg.test.tsx
@@ -1,53 +1,22 @@
 import React from 'react'
-import { render, fireEvent, act, wait } from '@testing-library/react'
+import { render, fireEvent, act } from '@testing-library/react'
 import { WarningMsg } from './WarningMsg'
 import { UnstatedContainer } from "./StateContainer"
-import { MemoryRouter } from 'react-router-dom'
-import { Initialize } from './Initialize'
-import { mocked } from 'ts-jest/utils'
-import { client } from '../lib/trpc'
-jest.mock('../lib/trpc.ts')
-jest.mock('../lib/analytics.ts')
+import { InitialDataContainer } from '../lib/unstated'
 
-const renderResult = async () => {
-  const utmFields = {
-    utm_source: 'source1234',      //eslint-disable-line @typescript-eslint/camelcase
-    utm_medium: 'medium1234',      //eslint-disable-line @typescript-eslint/camelcase
-    utm_campaign: 'campaign1234',  //eslint-disable-line @typescript-eslint/camelcase
-    utm_term: 'term1234',          //eslint-disable-line @typescript-eslint/camelcase
-    utm_content: 'content1234',    //eslint-disable-line @typescript-eslint/camelcase
-  }
-
-  const params = Object.entries(utmFields).map(([key, val]) => `${key}=${val}`).join('&')
-
-  const initialData = mocked(client, true).fetchInitialData = jest.fn().mockResolvedValue({
-    type: 'data',
-    data: { emailFaxOfficials: false, org: {} },
-  })
-
-  const rendered = render(
-    // Using MemoryRouter is recommended way to test react-router-dom
-    // https://reacttraining.com/react-router/web/guides/testing
-    <MemoryRouter initialEntries={['/org/default?' + params]}>
-      <Initialize/>
-      <WarningMsg/>
-    </MemoryRouter>,
-    { wrapper: UnstatedContainer },
-  )
-
-  await act(async () => {
-    await wait(() => expect(initialData).toHaveBeenCalledTimes(1))
-  })
-
-  return rendered
-}
+const renderResult = () => render(
+  <InitialDataContainer.Provider initialState={{ emailFaxOfficials: false, org: {} }}>
+    <WarningMsg/>
+  </InitialDataContainer.Provider>,
+  { wrapper: UnstatedContainer },
+)
 
 // When visiting the page for the first time
 
-test('Modal shows by default when visiting the page for the first time', async () => {
+test('Modal shows by default when visiting the page for the first time', () => {
   localStorage.removeItem('visited')
 
-  const rendered = await renderResult()
+  const rendered = renderResult()
 
   const modal = rendered.getByTestId('notProductionModal')
 
@@ -56,7 +25,7 @@ test('Modal shows by default when visiting the page for the first time', async (
 test('Dismissing the modal triggers a toast when visiting the page for the first time', async () => {
   localStorage.removeItem('visited')
 
-  const rendered = await renderResult()
+  const rendered = renderResult()
 
   const dismissButton = rendered.getByTestId('notProductionButton')
 
@@ -73,10 +42,10 @@ test('Dismissing the modal triggers a toast when visiting the page for the first
 
 // When visiting the page again
 
-test('Modal doesn\'t show by default when visiting the page again', async () => {
+test('Modal doesn\'t show by default when visiting the page again', () => {
   localStorage.setItem('visited', 'true')
 
-  const rendered = await renderResult()
+  const rendered = renderResult()
 
   const modal = rendered.queryByTestId('notProductionModal')
 
@@ -86,7 +55,7 @@ test('Modal doesn\'t show by default when visiting the page again', async () => 
 test('Dismissing the modal doesn\'t trigger toasts when visiting the page again', async () => {
   localStorage.setItem('visited', 'true')
 
-  const rendered = await renderResult()
+  const rendered = renderResult()
 
   const dismissButton = rendered.getByTestId('notProductionButton')
 
