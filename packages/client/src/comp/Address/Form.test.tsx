fed774b3cbeab6eb33cbaea4453a6e0422dea7a0
Lukas Danin <lukas.danin@gmail.com>
2020-08-17 16:40:55 -0300

    Fixes Form.test.tsx

--- packages/client/src/comp/Address/Form.test.tsx
+++ packages/client/src/comp/Address/Form.test.tsx
@@ -5,9 +5,18 @@ import { UnstatedContainer } from "../StateContainer"
 import { client } from '../../lib/trpc'
 import { pageView } from '../../lib/analytics'
 import { mocked } from 'ts-jest/utils'
+import { AddressInputParts } from '../../common'
 
 jest.mock('../../lib/analytics')
 jest.mock('../../lib/trpc')
+
+const mailingAddress: AddressInputParts = {
+  street: '100 Biscayne Blvd',
+  unit: '11',
+  city: 'Miami',
+  state: 'Florida',
+  postcode: '33131',
+}
 
 test('AddressForm works', async () => {
   const mockedPageView = mocked(pageView)
@@ -25,15 +34,32 @@ test('AddressForm works', async () => {
     { wrapper: UnstatedContainer }
   )
 
-  act(() => {
-    fireEvent.change(getByLabelText(/^Full Address/i), {
-      target: {
-        value: '100 S Biscayne Blvd, Miami, FL 33131'
-      },
+  await act(async () => {
+    const streetInput = getByLabelText(/^Street Address/i)
+    const apartmentInput = getByLabelText(/^Apartment/i)
+    const cityInput = getByLabelText(/^City/i)
+    const stateInput = getByTestId('addressFieldState').firstChild
+    const zipInput = getByLabelText(/^ZIP code/i)
+
+    await fireEvent.change(streetInput, {
+      target: { value: mailingAddress.street },
     })
-    fireEvent.click(getByTestId('submit'), {
-        bubbles: true,
-        cancelable: true,
+    await fireEvent.change(apartmentInput, {
+      target: { value: mailingAddress.unit },
+    })
+    await fireEvent.change(cityInput, {
+      target: { value: mailingAddress.city },
+    })
+    await fireEvent.change(stateInput, {
+      target: { value: mailingAddress.state },
+    })
+    await fireEvent.change(zipInput, {
+      target: { value: mailingAddress.postcode },
+    })
+
+    await fireEvent.click(getByTestId('submit'), {
+      bubbles: true,
+      cancelable: true,
     })
   })
 
