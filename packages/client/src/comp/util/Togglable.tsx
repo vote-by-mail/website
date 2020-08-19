19d8c21b1a622a7bc2f7deba7fc375d83ff38bb3
Lukas Danin <lukas.danin@gmail.com>
2020-08-19 17:27:59 -0300

    Normalizes MuiCSS onChange

--- packages/client/src/comp/util/Togglable.tsx
+++ packages/client/src/comp/util/Togglable.tsx
@@ -11,17 +11,28 @@ const useCheckbox = (init = false) => {
 }
 const CheckboxContainer = createContainer(useCheckbox)
 
-interface Props extends CheckboxProps {
+// To allow a custom onChange based on the boolean onChecked change
+interface BaseProps extends Omit<CheckboxProps, 'onChange'> {
   children: (checked: boolean) => React.ReactNode
 }
+
+type Props = BaseProps & { onChange?: (value: boolean) => void }
 
 const Wrapper = styled.div`
   padding-top: 15px;
   margin-bottom: 20px;
 `
 
-const RawTogglable: React.FC<Props> = ({children, ...props}) => {
+const RawTogglable: React.FC<Props> = ({children, onChange, ...props}) => {
   const { checked, toggleCheck } = CheckboxContainer.useContainer()
+
+  // Using React.useEffect to call this component props.onChange after
+  // after AppCheckbox.onChange, providing the desired result since when
+  // this prop is provided we're most likely wanting to perform calculations
+  // with the new value of checked
+  React.useEffect(() => {
+    if (onChange) onChange(checked)
+  }, [checked, onChange])
 
   // Matching style of input fields
   return <Wrapper>
