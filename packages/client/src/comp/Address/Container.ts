07c97fdca1b1dd7d916615c7cf30bf0a3743ea4d
Lukas Danin <lukas.danin@gmail.com>
2020-08-18 23:45:51 -0300

    Allows abbreviated states to be used

--- packages/client/src/comp/Address/Container.ts
+++ packages/client/src/comp/Address/Container.ts
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createContainer } from 'unstated-next'
-import { AddressInputParts, Address } from '../../common'
+import { AddressInputParts, Address, getState, isState } from '../../common'
 import { useAppHistory, StartSectionPath, AddressPath, StatePath, StateRedirectPath, SuccessPath } from '../../lib/path'
 import { AddressContainer } from '../../lib/unstated'
 
@@ -11,6 +11,20 @@ const removeOptionalFields = (fields: AddressInputParts) => ({
   unit: fields.unit ? fields.unit : undefined,
 })
 
+const handleDefaultState = (rawState: string) => {
+  // check if a state abbreviation is used
+  if (rawState.length === 2) {
+    const state = getState(rawState)
+    if (state) return state
+  }
+  if (isState(rawState)) {
+    return rawState
+  }
+
+  // Return an empty string to prevent errors with invalid states in the application
+  return ''
+}
+
 const handleDefault = (
   id: InputId,
   address: Address | null,
@@ -19,12 +33,13 @@ const handleDefault = (
 ): string => {
   if ((id === 'postcode' || id === 'state') && path?.type === 'address') {
     const normalizedId = id === 'state' ? id : 'zip'
-    return path[normalizedId]
+    const rawValue = path[normalizedId]
       ? path[normalizedId] as string
       : (address && address[id]) ?? query[id] ?? ''
+    return id === 'state' ? handleDefaultState(rawValue) : rawValue
   }
   if (id === 'state' && path?.type === 'state') {
-    return path.state ?? address?.state ?? query[id] ?? ''
+    return handleDefaultState(path.state ?? address?.state ?? query[id] ?? '')
   }
 
   return (address && address[id]) ?? query[id] ?? ''
