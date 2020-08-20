a9c2eabe19f3bbf28184e752eda0533a2d78f1de
Lukas Danin <lukas.danin@gmail.com>
2020-08-19 23:54:58 -0300

    Makes option a stateful value

--- packages/client/src/comp/states/Kansas.tsx
+++ packages/client/src/comp/states/Kansas.tsx
@@ -14,6 +14,7 @@ export const Kansas = () => {
   const [idPhoto, setIdPhoto] = React.useState<string>()
   const [idType, setIdType] = React.useState<'Number' | 'Image'>('Number')
   const [idNumber, setIdNumber] = React.useState<string>()
+  const [option, setOption] = React.useState<typeof options[number]>('Driver\'s License Number')
   const enrichValues = (baseInfo: StatelessInfo): NoSignature<KansasInfo> | null => {
     return {
       ...baseInfo,
@@ -24,42 +25,39 @@ export const Kansas = () => {
     }
   }
 
-  // Clears unused inputs
-  React.useEffect(() => {
-    if (idType === 'Image') {
-      setIdNumber(undefined)
-    } else {
-      setIdPhoto(undefined)
-    }
-  }, [idType, setIdNumber, setIdPhoto])
-
   return <SignatureBase<KansasInfo>enrichValues={enrichValues}>
     <TogglableDropdown<typeof options>
-      defaultValue={'Driver\'s License Number'}
+      defaultValue={option}
       label='Identification Method:'
       style={{ marginTop: 40 }}
       options={options}
-      onChange={value => setIdType(
-        value === 'Driver\'s License Number' ? 'Number' : 'Image'
-      )}
-    >{
-      (selected) => {
-        if (selected === 'Driver\'s License Number') {
-          return <BaseInput
-            id='idData'
-            label={selected}
-            required={true}
-            value={idNumber}
-            onChange={e => setIdNumber(e.currentTarget.value)}
-          />
+      value={option}
+      onChange={value => {
+        setOption(value)
+        setIdType(value === 'Driver\'s License Number' ? 'Number' : 'Image')
+
+        // Clears unused inputs
+        if (idType === 'Image') {
+          setIdNumber(undefined)
+        } else {
+          setIdPhoto(undefined)
         }
-        return (
-          <div>
-            <p>If I do not have a current valid Kansas driver’s license number or Kansas nondriver’s identification card number, I must provide a copy of one of the following forms of photo identification with this application in order to receive a ballot.</p>
-            <Upload label='Upload Photo of ID' setDataString={setIdPhoto} required={true}/>
-          </div>
-        )
+      }}
+    >{(selected) => {
+      switch (selected) {
+        case 'Copy of Photo ID': return <div>
+          <p>If I do not have a current valid Kansas driver’s license number or Kansas nondriver’s identification card number, I must provide a copy of one of the following forms of photo identification with this application in order to receive a ballot.</p>
+          <Upload label='Upload Photo of ID' setDataString={setIdPhoto} required={true}/>
+        </div>
+
+        default: return <BaseInput
+          id='idData'
+          label={selected}
+          required={true}
+          value={idNumber}
+          onChange={e => setIdNumber(e.currentTarget.value)}
+        />
       }
-    }</TogglableDropdown>
+    }}</TogglableDropdown>
   </SignatureBase>
 }
