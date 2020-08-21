f0f575cafedf204d85cf9a9c5c18dd06849e3bc4
Lukas Danin <lukas.danin@gmail.com>
2020-08-21 19:21:57 -0300

    Improves documentation & first time logic
    Sets cron.yaml time to UTC

--- packages/server/src/analytics/updateTimeSeries.ts
+++ packages/server/src/analytics/updateTimeSeries.ts
@@ -28,24 +28,38 @@ export const updateTimeSeries = async () => {
     lastQueryTime: storedLastQueryTime,
     yesterdaySignups: storedYesterdaySignups,
     totalSignups: storedTotalSignups,
+    yesterdayDate: storedYesterdayDate,
   } = await analyticsStorage.data()
 
   const stateInfos = firestore.db.collection('StateInfo')
 
   // We only select 'created' when doing these metrics, to avoid fetching
   // needless information
-  const query = stateInfos.where('created', '>=', storedLastQueryTime).select('created')
+  const query = stateInfos.where('created', '>', storedLastQueryTime).select('created')
   const snapshot = await query.get()
 
   let newYesterdaySignups = storedYesterdaySignups
   let newTotalSignups = storedTotalSignups
 
-  snapshot.forEach(
-    d => d.createTime.seconds > (startTime.valueOf() / 1000)
-      ? newYesterdaySignups++
-      : newTotalSignups++
-  )
-  newTotalSignups += (newYesterdaySignups - storedYesterdaySignups)
+  // We process this query differently based on the value of storedYesterdayDate.
+  //
+  // If `storedYesterdayDate === startTime` (happens on the first time this
+  // algorithm is run), we'll have to manually iterate through the array
+  // in order to detect which records happened yesterday.
+  //
+  // If storedYesterdayDate is actually yesterday, we can easily increment
+  // newYesterdaySignups/newTotalSignups based on the size of the snapshot.
+  if (storedYesterdayDate.valueOf() === startTime.valueOf()) {
+    snapshot.forEach(
+      d => d.createTime.seconds >= (startTime.valueOf() / 1000)
+        ? newYesterdaySignups++
+        : newTotalSignups++
+    )
+    newTotalSignups += (newYesterdaySignups - storedYesterdaySignups)
+  } else {
+    newYesterdaySignups += snapshot.size
+    newTotalSignups += snapshot.size
+  }
 
   await client.createTimeSeries({
     name: projectPath,
