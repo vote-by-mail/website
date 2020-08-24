8d8c8484cb46a26903618dab63a1b661a3d75448
Lukas Danin <lukas.danin@gmail.com>
2020-08-24 19:37:17 -0300

    Implements addressPartsToAddress

--- packages/common/address.ts
+++ packages/common/address.ts
@@ -1,3 +1,5 @@
+import { getStateAbbr, State } from './states'
+
 export interface Address {
   latLong?: [number, number]
   addressParts?: AddressInputParts
@@ -30,3 +32,17 @@ export const formatAddressInputParts = (addr: AddressInputParts): string => {
     ? `${addr.street} #${addr.unit}, ${addr.city}, ${addr.state} ${addr.postcode}`
     : `${addr.street}, ${addr.city}, ${addr.state} ${addr.postcode}`
 }
+
+/** Converts the given AddressInputParts to an Address interface */
+export const addressPartsToAddress = (addr: AddressInputParts): Address => ({
+  addressParts: addr,
+  country: 'US',
+  fullAddr: formatAddressInputParts(addr),
+  queryAddr: formatAddressInputParts(addr),
+  postcode: addr.postcode,
+  state: addr.state,
+  city: addr.city,
+  street: addr.street,
+  stateAbbr: getStateAbbr(addr.state as State),
+  unit: addr.unit,
+})
