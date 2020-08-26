8ae2a358aaf9fc7a47debb26daeee0fe839fe71c
Lukas Danin <lukas.danin@gmail.com>
2020-08-26 17:51:57 -0300

    Implements possibleStreetNumbers

--- packages/common/address.ts
+++ packages/common/address.ts
@@ -33,16 +33,38 @@ export const formatAddressInputParts = (addr: AddressInputParts): string => {
     : `${addr.street}, ${addr.city}, ${addr.state} ${addr.postcode}`
 }
 
+/**
+ * Returns all found numbers in the given street address, return null if
+ * none is found.
+ *
+ * This function can return more than one possible street number, in these
+ * cases users must select their street number from this list manually.
+ */
+export const possibleStreetNumbers = (street: string): string[] | null => {
+  // Checks for numbers between word boundaries, so cases like 5th avenue
+  // are ignored.
+  const match = street.match(/\b([0-9]+)\b/g)
+  return match
+}
+
+
 /** Converts the given AddressInputParts to an Address interface */
-export const addressPartsToAddress = (addr: AddressInputParts): Address => ({
-  addressParts: addr,
-  country: 'US',
-  fullAddr: formatAddressInputParts(addr),
-  queryAddr: formatAddressInputParts(addr),
-  postcode: addr.postcode,
-  state: addr.state,
-  city: addr.city,
-  street: addr.street,
-  stateAbbr: getStateAbbr(addr.state as State),
-  unit: addr.unit,
-})
+export const addressPartsToAddress = (addr: AddressInputParts): Address => {
+  const streetNumbers = possibleStreetNumbers(addr.street)
+
+  return {
+    addressParts: addr,
+    country: 'US',
+    fullAddr: formatAddressInputParts(addr),
+    queryAddr: formatAddressInputParts(addr),
+    postcode: addr.postcode,
+    state: addr.state,
+    city: addr.city,
+    street: addr.street,
+    stateAbbr: getStateAbbr(addr.state as State),
+    // If more than one streetNumber, we should let users manually pick
+    // them on the front-end.
+    streetNumber: streetNumbers?.length === 1 ? streetNumbers[0] : undefined,
+    unit: addr.unit,
+  }
+}
