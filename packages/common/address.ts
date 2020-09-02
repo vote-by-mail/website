80163f72624172894c67fd8259211096d2500e91
Lukas Danin <lukas.danin@gmail.com>
2020-09-02 16:04:36 -0300

    No longer formats units
    It's unlikely that people are going to type more than just numbers in
    this field

--- packages/common/address.ts
+++ packages/common/address.ts
@@ -27,33 +27,9 @@ export interface AddressInputParts {
   unit?: string
 }
 
-/** Removes unnecessary and trailing spacing */
-const trim = (s: string) => s.replace(/( +)/g, ' ').trim()
-
-/** Returns a formatted unit, using pound (#) only when necessary */
-export const formatUnit = (unit: string) => {
-  // Users might use pounds erroneously, let this function decide whether
-  // to use it or not.
-  //
-  // We also remove unnecessary spacing, for example the double spacing
-  // in 'BLDG  3'
-  const normalized = trim(unit.replace(/#/g, ''))
-
-  // Searchs for any abbreviation that don't require pounds
-  //
-  // The list of officially approved abbreviations can be found here: https://pe.usps.com/text/pub28/28apc_003.htm#ep538629
-  // But we'll remove pounds if any word is found in the Unit field, since users
-  // might not use the officially approved abbreviations or just commit a typo
-  // while filling this field, e.g. APY 303.
-  const dontUsePound = /( +|^)([a-z]+)( +|$)/i
-  return !normalized.toUpperCase().match(dontUsePound)
-    ? trim(`# ${normalized}`)
-    : normalized
-}
-
 export const formatAddressInputParts = (addr: AddressInputParts): string => {
   return addr.unit
-    ? `${addr.street} ${formatUnit(addr.unit)}, ${addr.city}, ${addr.state} ${addr.postcode}`
+    ? `${addr.street} # ${addr.unit}, ${addr.city}, ${addr.state} ${addr.postcode}`
     : `${addr.street}, ${addr.city}, ${addr.state} ${addr.postcode}`
 }
 
