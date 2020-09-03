2c84519dc3fbecc8ee16e209c7385a6ce5260d87
Lukas Danin <lukas.danin@gmail.com>
2020-09-02 22:52:07 -0300

    Improves type inference related to webhooks

---
+++ packages/server/src/service/webhooks.d.ts
@@ -0,0 +1,72 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export type MailgunLogLevel = 'info' | 'warn' | 'temporary' | 'error'
+export type GCPLogLevel = 'INFO' | 'WARNING' | 'ERROR'
+
+// Currently (at Sep 2nd 2020), @types/mailgun-js does not yet provide
+// rich information about the two interfaces here. These STUB definitions
+// will help us work around this limitation.
+
+export interface MailgunEventData {
+  severity: string
+  tags: string[]
+  /** Seconds since January 1st, 1970 */
+  timestamp: number
+  storage: { url: string, key: string }
+  'log-level': MailgunLogLevel
+  id: string
+  campaigns: string[]
+  reason: string
+  'user-variables': any
+  flags: {
+    'is-routed': boolean
+    'is-authenticated': boolean
+    'is-system-test': boolean
+    'is-test-mode': boolean
+  }
+  envelope: {
+    /** Example: me@mydomain.com */
+    sender: string
+    transport: 'smtp'
+    /** Example: bob@gmail.com */
+    targets: string
+  }
+
+  /** Example: bob@gmail.com */
+  recipient: string
+  /** Example: 'gmail.com' if envelope.target/recipient is 'bob@gmail.com' */
+  'recipient-domain': string
+
+  message: {
+    headers: Record<string, any>
+    attachments: string[]
+    size: number
+  }
+
+  event:
+    | 'accepted' | 'rejected' | 'delivered' | 'failed'
+    | 'opened' | 'clicked' | 'unsubscribed' | 'complained'
+    | 'stored' | 'list_member_uploaded' | 'list_member_upload_error'
+    | 'list_uploaded'
+
+  'delivery-status': {
+    'attempt-no': number
+    message: string
+    code: number
+    description: string
+    'session-seconds': number
+  }
+}
+
+export interface MailgunHookBody {
+  /**
+   * Used to prove the authenticy of MG webhook requests
+   */
+  signature: {
+    /** Seconds since January 1st, 1970 */
+    timestamp: number
+    token: string
+    signature: string
+  }
+
+  'event-data': MailgunEventData
+}
