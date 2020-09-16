5f51dad92f6f2cc413566f91aaec788938d1dc64
Lukas Danin <lukas.danin@gmail.com>
2020-09-16 11:27:22 -0300

    Renames loadEmailTesting

--- packages/server/src/script/loadRegistrationTesting.ts
+++ packages/server/src/script/loadRegistrationTesting.ts
@@ -24,22 +24,17 @@ const delay = async <T>(fn: () => Promise<T>, ms: number): Promise<T> => {
 let timeouts = 0
 let errors = 0
 
-/** Store the amount of timeouts and errors that happened in this execution */
+/** Store the amount of timeouts and errors that happened in this test */
 const storeLog = async () => {
   const file = `${__dirname}/data/loadEmailTesting.log`
   const content =
-`In the last execution ${timeouts} timeouts happened, and ${errors} errors occurred.`
+`In the last test ${timeouts} timeouts happened, and ${errors} errors occurred.`
 
   return fs.writeFile(file, content, { encoding: 'utf-8' })
 }
 
 /**
  * Store all returned IDs so we can fetch their status later on.
- *
- * This is needed since MG API uses a queue-based system, because of this,
- * messages might take some time to arrive in an inbox--and only when they
- * arrive (or fail to arrive) is that MG updates its events logging, which
- * we'll use to check if the registrations were successful.
  *
  * @param qps The .json file is named after the qps, so we can store the result
  * of multiple batches simultaneously
@@ -70,19 +65,19 @@ const storeCycleResults = async (
 }
 
 /**
- * Send N amount of emails per second through a given duration.
+ * Send N amount of registrations per second through a given duration.
  *
- * @param qps Amount of emails to be sent per seconds
+ * @param qps Amount of registrations to be sent per seconds
  * @param duration Duration of this cycle in seconds
  */
-const loadEmailTesting = async (qps: number, duration: number) => {
+const loadRegistrationTesting = async (qps: number, duration: number) => {
   const startTime = new Date()
   const stateInfo = await sampleStateInfo('Florida')
   stateInfo.email = fakeEmail
   // Creates an array that has an interval of one second until we reach duration
   const startTimes = Array(duration).fill(0).map((_, index) => index * 1000)
 
-  console.log(`Starting sending cycle of ${qps * duration} emails (queries per second: ${qps}, duration: ${duration} seconds) to test MG limits.`)
+  console.log(`Starting sending cycle of ${qps * duration} registrations (queries per second: ${qps}, duration: ${duration} seconds) to test MG limits.`)
 
 
   // Holds promises acquired from each qps cycle so we can process them
@@ -111,7 +106,7 @@ const loadEmailTesting = async (qps: number, duration: number) => {
 
       const seconds = time / 1000
       if (seconds && seconds % 5 === 0) console.log(`${seconds} seconds have passed since the beginning of this cyle.`)
-
+      if (seconds === duration - 1) console.log('Cycle finished, waiting for promises...')
       return Promise.all(qpsCycle)
     },
     time,
@@ -131,17 +126,17 @@ const main = async () => {
   await contactRecords
 
   // One query per second
-  await loadEmailTesting(1, 20)
+  await loadRegistrationTesting(1, 20)
   // Three queries per second
-  await loadEmailTesting(3, 20)
+  await loadRegistrationTesting(3, 20)
   // Ten queries per second
-  await loadEmailTesting(10, 20)
+  await loadRegistrationTesting(10, 20)
 
   console.log(`Total timeouts: ${timeouts}, total internal errors: ${errors}. Saving log...`)
   await storeLog()
   console.log(`Saved this information at ${__dirname}/data/loadEmailTesting.log`)
 
-  console.log('When MG finishes sending the messages from the queue, be sure to run ./verifyLoadTesting.ts script to verify their sending/rejected status.')
+  console.log('IDs and errors where stored in the data folder, you can run ./verifyLoadTesting.ts to get accepted/rejected statuses (it might take a while until all registrations are sent from used API\'s queues).')
 }
 
 main()
