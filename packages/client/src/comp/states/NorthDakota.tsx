e32d029a3a80d7f96f9bd02b86736e2f7118440b
Lukas Danin <lukas.danin@gmail.com>
2020-09-21 20:06:49 -0300

    Converts ND to use controlled components

--- packages/client/src/comp/states/NorthDakota.tsx
+++ packages/client/src/comp/states/NorthDakota.tsx
@@ -1,60 +1,47 @@
 import React from 'react'
 
-import { useControlRef } from '../util/ControlRef'
-import { TogglableDropdown } from '../util/TogglableDropdown'
+import { Select } from '../util/Select'
 import { BaseInput } from '../util/Input'
 
 import { SignatureBase, StatelessInfo, NoSignature } from './Base'
-import { NorthDakotaInfo, NorthDakotaIdentityType, northDakotaIdentityType, northDakotaElectionType, isNorthDakotaElectionType } from '../../common'
-
-import Input from 'muicss/lib/react/input'
-import Select from 'muicss/lib/react/select'
-import Option from 'muicss/lib/react/option'
+import { NorthDakotaInfo, NorthDakotaIdentityType, northDakotaIdentityType, northDakotaElectionType, NorthDakotaElectionType } from '../../common'
 
 export const NorthDakota = () => {
-    const electionTypeRef = useControlRef<Input>()
-    const [option, setOption] = React.useState<NorthDakotaIdentityType>(northDakotaIdentityType[0])
-    const [idNumber, setIdNumber] = React.useState<string>()
+  const [electionType, setElectionType] = React.useState<NorthDakotaElectionType>('Both')
+  const [idType, setIdType] = React.useState<NorthDakotaIdentityType>('Non Driver ID')
+  const [idNumber, setIdNumber] = React.useState<string>('')
 
-    const enrichValues = (baseInfo: StatelessInfo): NoSignature<NorthDakotaInfo> | null => {
-        const electionType: string | null = electionTypeRef.value()
-        if (!isNorthDakotaElectionType(electionType)) return null
-        if (idNumber === undefined) return null
-        return {
-            ...baseInfo,
-            state: 'North Dakota',
-            electionType,
-            idType: option,
-            idNumber 
-        }
+  const enrichValues = (baseInfo: StatelessInfo): NoSignature<NorthDakotaInfo> | null => {
+    if (!idNumber) return null
+    return {
+      ...baseInfo,
+      state: 'North Dakota',
+      electionType,
+      idType,
+      idNumber
     }
+  }
 
-    return <SignatureBase<NorthDakotaInfo>enrichValues={enrichValues}>
-    <TogglableDropdown<typeof northDakotaIdentityType>
+  return <SignatureBase<NorthDakotaInfo> enrichValues={enrichValues}>
+    <Select
       label='Identification Method:'
       style={{ marginTop: 40 }}
       options={northDakotaIdentityType}
-      value={option}
-      onChange={value => {
-        setOption(value)
-      }}
-    >{(option) => {
-      switch (option) {
-        default: return <BaseInput
-          id='idData'
-          label={option}
-          required={true}
-          value={idNumber}
-          onChange={e => setIdNumber(e.currentTarget.value)}
-        />
-      }
-    }}
-    </TogglableDropdown>
-    <Select ref={electionTypeRef} label = 'Election Type for Mail in Ballot' defaultValue='Select' {...{required: true}}>
-      <Option key={0} hidden={true}/>
-      {[...northDakotaElectionType].sort().map((value, key) => {
-        return <Option value={value} key={key+1} label={value}/>
-      })}
-    </Select>
+      value={idType}
+      onChange={value => setIdType(value)}
+    />
+    <BaseInput
+      id='idData'
+      label={idType}
+      required={true}
+      value={idNumber}
+      onChange={e => setIdNumber(e.currentTarget.value)}
+    />
+    <Select
+      label='Election Type for Mail in Ballot'
+      options={northDakotaElectionType}
+      value={electionType}
+      onChange={e => setElectionType(e)}
+    />
   </SignatureBase>
 }
