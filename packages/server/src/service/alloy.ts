8e147de78a906c9eb36ddca3310800461577045d
Lukas Danin <lukas.danin@gmail.com>
2020-09-22 20:08:48 -0300

    Allows to fetch Alloy registration by Alloy ID

--- packages/server/src/service/alloy.ts
+++ packages/server/src/service/alloy.ts
@@ -1,5 +1,5 @@
 import { processEnvOrThrow, RegistrationStatus, RegistrationArgs, allRegistrationStatus, AlloyResponse } from '../common'
-import axios, { AxiosResponse } from 'axios'
+import axios, { AxiosResponse, AxiosRequestConfig } from 'axios'
 import rax from 'retry-axios'
 import { cache } from './util'
 
@@ -24,6 +24,26 @@ interface AxiosAlloyResponse {
 const lowercaseStatuses = allRegistrationStatus.map(x => x.toLowerCase())
 
 /**
+ * 429 means we are firing more requests than our maximum rate per second,
+ * to avoid alerting users about this, we use retry-axios on this request
+ */
+const repeatAxios = async (requestConfig: AxiosRequestConfig): Promise<AxiosAlloyResponse> => {
+  const axiosInstance = axios.create()
+  rax.attach(axiosInstance)
+  const response = await axiosInstance({
+    ...requestConfig,
+    raxConfig: {
+      retry: 4,
+      noResponseRetries: 0,
+      statusCodesToRetry: [[429, 429]],
+      backoffType: 'exponential',
+    },
+  }) as AxiosResponse<AxiosAlloyResponse>
+
+  return response.data
+}
+
+/**
  * Converts MM/DD/YYYY dates to YYYY-MM-DD, since Alloy only accepts this
  * date-format.
  *
@@ -36,6 +56,10 @@ export const toAlloyDate = (vbmBirthdate: string) => {
   return `${split[2]}-${split[0]}-${split[1]}`
 }
 
+/**
+ * Fetches the registration status of a voter by manually querying the
+ * records with the information from StateInfo
+ */
 export const isRegistered = async ({
   nameParts, birthdate,
   stateAbbr, city, postcode,
@@ -72,24 +96,11 @@ export const isRegistered = async ({
   ].filter(str => !!str).join('&')
   const url = `${apiUrl}/verify?${query}`
 
-  // 429 means we are firing more requests than our maximum rate per second,
-  // to avoid alerting users about this, we use retry-axios on this request
-  const axiosInstance = axios.create()
-  rax.attach(axiosInstance)
-  const response = await axiosInstance({
-    ...commonAxiosSettings,
-    url,
-    raxConfig: {
-      retry: 4,
-      noResponseRetries: 0,
-      statusCodesToRetry: [[429, 429]],
-      backoffType: 'exponential',
-    }
-  }) as AxiosResponse<AxiosAlloyResponse>
+  const response = await repeatAxios({...commonAxiosSettings, url})
 
-  const id = response.data.data.id
+  const id = response.data.id
   // When not found Alloy API returns null, we default this to Not Found
-  const status =  response.data.data.registration_status ?? 'Not Found'
+  const status =  response.data.registration_status ?? 'Not Found'
 
   return { status, id }
 }
@@ -98,3 +109,21 @@ export const cacheIsRegistered = cache(
   isRegistered,
   async x => `Voter ${x.nameParts.first} ${x.nameParts.last}`,
 )
+
+/**
+ * Fetches the registration status of a voter by its Alloy ID
+ */
+export const isRegisteredByAlloyId = async (id: string): Promise<RegistrationStatus> => {
+  const url = `${apiUrl}/voters/${id}`
+
+  try {
+    const response = await repeatAxios({...commonAxiosSettings, url})
+    // When not found Alloy API returns null, we default this to Not Found
+    return response.data.registration_status ?? 'Not Found'
+  } catch(error) {
+    // We don't use console.error here since this might be a simple timeout
+    // error or exceeding the maximum amount of requests
+    console.warn(error)
+    return 'Error'
+  }
+}
