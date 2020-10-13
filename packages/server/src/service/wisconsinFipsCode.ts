d650f983c67a0ec162a5eda8eb729c7015dfc42b
Lukas Danin <lukas.danin@gmail.com>
2020-10-12 21:18:16 -0300

    Replaces open server with a private one

--- packages/server/src/service/wisconsinFipsCode.ts
+++ packages/server/src/service/wisconsinFipsCode.ts
@@ -11,7 +11,7 @@ interface Response {
 
 const rawWisconsinResponse = async (latLong: [number, number]): Promise<Response | null> => {
   const [lat, long] = latLong
-  const url = `https://mapservices.legis.wisconsin.gov/arcgis/rest/services/Statewide/Municipal_Boundaries_July_2020/FeatureServer/0/query?where=1%3D1&outFields=*&geometry=${long}%2C${lat}&geometryType=esriGeometryPoint&inSR=4326&spatialRel=esriSpatialRelIntersects&outSR=4326&f=json`
+  const url = `https://services5.arcgis.com/AbZVanP1q8d5OLCX/ArcGIS/rest/services/wisconsin/FeatureServer/0/query?where=&objectIds=&time=&geometry=${long}%2C${lat}4&geometryType=esriGeometryPoint&inSR=4236&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=DOA&returnGeometry=true&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=4326&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token=`
   try {
     return (await axios(url)).data as Response
   } catch (error) {
@@ -30,8 +30,7 @@ export const wisconsinFipsCode = async (
   latLong: [number, number],
   {cacheQuery} = {cacheQuery: false},
 ): Promise<string | null> => {
-  const k = false
-  const call = cacheQuery && k ? cacheWisconsinResponse : rawWisconsinResponse
+  const call = cacheQuery ? cacheWisconsinResponse : rawWisconsinResponse
   const response = await call(latLong)
   if (!response) return null
   return toFipscode(response)
